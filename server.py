881456e64f7de26b9cbbb7996c2f7917676b4aac
NunTheeKnee <jane6677doe@gmail.com>
2025-08-22 19:54:37 +0530

    Added the prometheus forfetching the metrics

--- server.py
+++ server.py
@@ -1,9 +1,11 @@
 import random
-from fastapi import FastAPI, APIRouter
-from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
+import prometheus_client
+from fastapi import FastAPI, APIRouter, Response
+from fastapi.middleware.cors import CORSMiddleware
+from time import time
 
-app = FastAPI()
+app = FastAPI(debug=True)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -12,8 +14,36 @@ app.add_middleware(
 )
 router = APIRouter()
 
+request_count = prometheus_client.Counter(
+    'requests_total', 'Requests count', ['method', 'endpoint']
+)
+
+request_latency = prometheus_client.Histogram(
+    'requests_latency', 'Request duration', ['method', 'endpoint']
+)
+
+@app.middleware("http")
+async def middle_func(request, call_next):
+    start_time = time()
+    response = await call_next(request)
+    duration = time() - start_time
+    method = request.method
+    endpoint = request.url
+    print(f"Request: {method} {endpoint} completed in {duration}s")
+    request_count.labels(method=method, endpoint=endpoint).inc()
+    request_latency.labels(method=method, endpoint=endpoint).observe(duration)
+    return response
+
+@app.get("/metrics")
+async def metrics():
+    return Response(
+        prometheus_client.generate_latest(), 
+        media_type="text/plain"
+    )
+
+# Routes
 @router.get("/")
-def root():
+async def root():
     return {"Hello": f"This is calling from Nandini's server {random.randint(1,100)}"}
 
 app.include_router(router=router)
